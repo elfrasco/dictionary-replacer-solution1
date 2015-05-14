d63d8dee835b25d75c717bb49a8664e43a797111
elfrasco <adrianparedes82@gmail.com>
2015-05-14 20:42:39 -0300

    Simple resolution!

--- src/main/java/co/centauri/tdd/kata/replacer/DictionaryReplacer.java
+++ src/main/java/co/centauri/tdd/kata/replacer/DictionaryReplacer.java
@@ -2,18 +2,54 @@ package co.centauri.tdd.kata.replacer;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class DictionaryReplacer {
 
+    private Map<String, String> context;
+    private String result;
+
     public String replace(String strToReplace, Map<String, String> context) {
-        switch (strToReplace) {
-            case "":
-                return "";
-            case "${temp}":
-                return "temporary";
-            case "${temp} here comes the name ${name}":
-                return "temporary here comes the name John Doe";
+        
+        this.context = context;
+        this.result = strToReplace;        
+        
+        if (nothingToDo()) {
+            return strToReplace;
         }
 
-        return null;
+        int begin = result.indexOf("${");
+        while (thereIs(begin)) {
+            replaceNext(begin);
+            begin = result.indexOf("${", begin + 2);
+        }
+        return result;
+    }
+
+    private boolean nothingToDo() {
+        return StringUtils.isBlank(result) || isContextEmpty();
+    }
+
+    private boolean isContextEmpty() {
+        return context == null || context.isEmpty();
+    }
+    
+    private boolean thereIs(int begin) {
+        return begin != -1;
+    }
+
+    private void replaceNext(int begin) {
+        int end = result.indexOf("}", begin);
+        if (thereIs(end)) {
+            String key = result.substring(begin + 2, end);
+            replace(key);
+        }
+    }
+
+    private void replace(String key) {
+        String value = this.context.get(key);
+        if (value != null) {
+            result = result.replaceAll("\\$\\{" + key + "\\}", value);
+        }
     }
 }
--- src/test/java/co/centauri/tdd/kata/replacer/DictionaryReplacerTest.java
+++ src/test/java/co/centauri/tdd/kata/replacer/DictionaryReplacerTest.java
@@ -42,21 +42,71 @@ public class DictionaryReplacerTest {
         context.put("temp", "temporary");
         return context;
     }
-    
+
     @Test
     public void twoParams() {
         String strToReplace = "${temp} here comes the name ${name}";
         Map<String, String> context = createContextWithTwoParams();
 
         String result = replacer.replace(strToReplace, context);
-        
+
         Assert.assertEquals("temporary here comes the name John Doe", result);
     }
-    
+
     private Map<String, String> createContextWithTwoParams() {
         Map<String, String> context = createContextWithOneParam();
         context.put("name", "John Doe");
         return context;
     }
-    
+
+    @Test
+    public void invalidPlaceholder() {
+        String strToReplace = "Hello ${Mr. Jones! How are you?";
+        Map<String, String> context = createContextWithTwoParams();
+
+        String result = replacer.replace(strToReplace, context);
+
+        Assert.assertEquals("Hello ${Mr. Jones! How are you?", result);
+    }
+
+    @Test
+    public void variableNotFound() {
+        String strToReplace = "Hello ${name}! How are you?";
+        Map<String, String> context = Collections.emptyMap();
+
+        String result = replacer.replace(strToReplace, context);
+
+        Assert.assertEquals("Hello ${name}! How are you?", result);
+    }
+
+    @Test
+    public void oneVariableFoundAndAnotherNotFound() {
+        String strToReplace = "Hello ${name}! How are you? You are the ${role} in this project.";
+        Map<String, String> context = createContextWithTwoParams();
+
+        String result = replacer.replace(strToReplace, context);
+
+        Assert.assertEquals("Hello John Doe! How are you? You are the ${role} in this project.", result);
+    }
+
+    @Test
+    public void nullStrToReplace() {
+        String strToReplace = null;
+        Map<String, String> context = createContextWithTwoParams();
+
+        String result = replacer.replace(strToReplace, context);
+
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void nullContext() {
+        String strToReplace = "Invalid ${context}";
+        Map<String, String> context = null;
+
+        String result = replacer.replace(strToReplace, context);
+
+        Assert.assertEquals("Invalid ${context}", result);
+    }
+
 }
